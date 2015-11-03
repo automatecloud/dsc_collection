c9c6f121f4947283f1cf2527b3cc30fa1604c720
Andulla <andreas.wilke@puppetlabs.com>
2015-11-03 09:59:35 +0100

    updated config.pp
    give warning messages if you try to use it with Windows Server 2008 R2
    or other operating systems

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,36 +1,48 @@
 # === Authors
 #
 # Andreas Wilke <andreas@puppetlabs.com>
-#
+# It only works with Windows 2012R2. With Windows 2008R2 it throws an error.
+
 class dsc_collection::config (
   # Refresh Mode = Disabled, Pull or Push
   $refresh_mode = 'Disabled'
   ) {
-    service {'wuauserv':
-      ensure => 'running',
-      enable => true,
-    }
-    # Install package powershell with chocolatey.
-    # And workaround for chocolatey provider
-    # as it will identify PowerShell as not installed.
-    if $::chocolatey_packages =~ /PowerShell 5.0.10514-ProductionPreview/ {
-      #Powershell already installed.
-      #notify { "Package is already installed":}
-    }
-    else
-    {
-      #Not install will use package to install
-      package { 'powershell':
-        ensure          => installed,
-        provider        => 'chocolatey',
-        install_options => ['-pre','-y'],
+    if $::kernelmajversion == 6.3 {
+      service {'wuauserv':
+        ensure => 'running',
+        enable => true,
       }
-      reboot { 'after':
-          subscribe       => Package['powershell'],
+      # Install package powershell with chocolatey.
+      # And workaround for chocolatey provider
+      # as it will identify PowerShell as not installed.
+      if $::chocolatey_packages =~ /PowerShell 5.0.10514-ProductionPreview/ {
+        #Powershell already installed.
+        #notify { "Package is already installed":}
       }
-    }
-  # Disable local refresh mode
-  dsc::lcm_config { 'disable lcm':
-    refresh_mode => $refresh_mode,
+      else
+      {
+        #Not install will use package to install
+        package { 'powershell':
+          ensure          => installed,
+          provider        => 'chocolatey',
+          install_options => ['-pre','-y'],
+        }
+        reboot { 'after':
+            subscribe       => Package['powershell'],
+        }
+      }
+      # Disable local refresh mode
+      dsc::lcm_config { 'disable lcm':
+      refresh_mode => $refresh_mode,
+      }
+  }
+  elsif $::kernelmajversion == 6.1 {
+    warning( 'This module does not yet work on Windows Server 2008R2.')
+    warning( 'Please download and install the necessary')
+    warning( 'Windows Management Framework 5.0 for Windows 2008R2.' )
+  }
+  else {
+    warning( 'Your operatingsystem is currently not supported with this module')
+    warning( 'Please do not use the config manifest file')
   }
 }
