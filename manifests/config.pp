502ca4d353b9f1886a8026d72ff015f5d33c349d
Andulla <andreas.wilke@puppetlabs.com>
2016-04-16 08:09:49 +0200

    Now possible to use only refresh mode configuration.

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,13 +5,63 @@
 
 class dsc_collection::config (
   # Refresh Mode = Disabled, Pull or Push
-  $refresh_mode = 'Disabled'
+  $refresh_mode = 'Disabled',
+  $onlyrefreshmode = false,
   ) {
     if $::kernelmajversion == '6.3' {
       service {'wuauserv':
         ensure => 'running',
         enable => true,
       }
+      if $onlyrefreshmode == true {
+        dsc::lcm_config { 'disable lcm':
+          refresh_mode => $refresh_mode,
+        }
+      }
+      else {
+        # Install package powershell with chocolatey.
+        # And workaround for chocolatey provider
+        # as it will identify PowerShell as not installed.
+        if $::chocolatey_packages != undef
+        {
+          if $::chocolatey_packages =~ /PowerShell 5.0/ {
+            #Powershell already installed.
+            # Disable local refresh mode
+            dsc::lcm_config { 'disable lcm':
+              refresh_mode => $refresh_mode,
+            }
+          }
+          else
+          {
+            #Not install will use package to install
+            package { 'powershell':
+              ensure          => installed,
+              provider        => 'chocolatey',
+              install_options => ['-pre','-y'],
+            }
+            reboot { 'after':
+                subscribe       => Package['powershell'],
+            }
+          }
+      }
+      else
+      {
+          warning ('Chocolatey is not installed so far.')
+          warning ('Please make sure you installed Chocolatey')
+      }
+    }
+  }
+  elsif $::kernelmajversion == '6.2' {
+    service {'wuauserv':
+      ensure => 'running',
+      enable => true,
+    }
+    if $onlyrefreshmode == true {
+      dsc::lcm_config { 'disable lcm':
+        refresh_mode => $refresh_mode,
+      }
+    }
+    else {
       # Install package powershell with chocolatey.
       # And workaround for chocolatey provider
       # as it will identify PowerShell as not installed.
@@ -27,9 +77,14 @@ class dsc_collection::config (
         else
         {
           #Not install will use package to install
+          package { 'dotnet4.5.1':
+            ensure   => installed,
+            provider => 'chocolatey',
+          }
           package { 'powershell':
             ensure          => installed,
             provider        => 'chocolatey',
+            require         => Package['dotnet4.5.1'],
             install_options => ['-pre','-y'],
           }
           reboot { 'after':
@@ -43,46 +98,6 @@ class dsc_collection::config (
         warning ('Please make sure you installed Chocolatey')
     }
   }
-  elsif $::kernelmajversion == '6.2' {
-    service {'wuauserv':
-      ensure => 'running',
-      enable => true,
-    }
-    # Install package powershell with chocolatey.
-    # And workaround for chocolatey provider
-    # as it will identify PowerShell as not installed.
-    if $::chocolatey_packages != undef
-    {
-      if $::chocolatey_packages =~ /PowerShell 5.0/ {
-        #Powershell already installed.
-        # Disable local refresh mode
-        dsc::lcm_config { 'disable lcm':
-          refresh_mode => $refresh_mode,
-        }
-      }
-      else
-      {
-        #Not install will use package to install
-        package { 'dotnet4.5.1':
-          ensure   => installed,
-          provider => 'chocolatey',
-        }
-        package { 'powershell':
-          ensure          => installed,
-          provider        => 'chocolatey',
-          require         => Package['dotnet4.5.1'],
-          install_options => ['-pre','-y'],
-        }
-        reboot { 'after':
-            subscribe       => Package['powershell'],
-        }
-      }
-  }
-  else
-  {
-      warning ('Chocolatey is not installed so far.')
-      warning ('Please make sure you installed Chocolatey')
-  }
 }
   elsif $::kernelmajversion == '6.1' {
     warning( 'You are using this module with Windows Server 2008R2.')
@@ -90,38 +105,45 @@ class dsc_collection::config (
     # Install package powershell with chocolatey.
     # And workaround for chocolatey provider
     # as it will identify PowerShell as not installed.
-    if $::chocolatey_packages != undef
-    {
-      if $::chocolatey_packages =~ /PowerShell 5.0/ {
-        #Powershell already installed.
-        dsc::lcm_config { 'disable lcm':
-          refresh_mode => $refresh_mode,
+    if $onlyrefreshmode == true {
+      dsc::lcm_config { 'disable lcm':
+        refresh_mode => $refresh_mode,
+      }
+    }
+    else {
+      if $::chocolatey_packages != undef
+      {
+        if $::chocolatey_packages =~ /PowerShell 5.0/ {
+          #Powershell already installed.
+          dsc::lcm_config { 'disable lcm':
+            refresh_mode => $refresh_mode,
+          }
+        }
+        else
+        {
+          #Not install will use package to install
+          # Need to install .Net 4.5.1 first.
+          package { 'dotnet4.5.1':
+            ensure   => installed,
+            provider => 'chocolatey',
+          }
+
+          package { 'powershell':
+            ensure          => installed,
+            provider        => 'chocolatey',
+            require         => Package['dotnet4.5.1'],
+            install_options => ['-pre','-y'],
+          }
+          reboot { 'after':
+              subscribe       => Package['powershell'],
+          }
         }
       }
       else
       {
-        #Not install will use package to install
-        # Need to install .Net 4.5.1 first.
-        package { 'dotnet4.5.1':
-          ensure   => installed,
-          provider => 'chocolatey',
-        }
-
-        package { 'powershell':
-          ensure          => installed,
-          provider        => 'chocolatey',
-          require         => Package['dotnet4.5.1'],
-          install_options => ['-pre','-y'],
-        }
-        reboot { 'after':
-            subscribe       => Package['powershell'],
-        }
+        warning ('Chocolatey is not installed so far.')
+        warning ('Please make sure you installed Chocolatey')
       }
-    }
-    else
-    {
-      warning ('Chocolatey is not installed so far.')
-      warning ('Please make sure you installed Chocolatey')
     }
   }
   else {
